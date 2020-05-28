c0c2a569519d8ba3a7c67dacf99970c7e5a4fd68
gkumar9891 <54814034+gkumar9891@users.noreply.github.com>
2020-05-28 13:44:18 +0530

    added some file

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_POST["submit"]))
+{
+   $name = $_POST["name"];	
+	$subject = $_POST["subject"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+	
+    $mail = "gauravkumargk9891@gmail.com";
+    $headers = "From: ". $email;
+		$txt = "You have reaceved an email from ". $email. "\n\n". $message;
+		
+	mail($mail, $subject, $txt, $headers);
+	header("location: index.php?mailsuccess");
+
+}
+
+?>

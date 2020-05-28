c654b51735079a41560863dd5a3adae93eee66c3
gkumar9891 <54814034+gkumar9891@users.noreply.github.com>
2020-05-28 14:32:49 +0530

    update

--- mail.php
+++ mail.php
@@ -10,12 +10,8 @@ if(isset($_GET["submit"]))
     $mailTo = "gauravkumargk9891@gmail.com";
     $headers = "From: ". $email;
 		$txt = "You have reaceved an email from ". $email. "\n\n". $message;
-	
 	mail($mailTo, $subject, $txt, $headers);
 	$msg = "<div>Sent successful</div>"
-
-
-
 }
 
 ?>

03de1522768d567f10d7a4e390e5720b318082d1
gkumar9891 <gauravkumargk9892@gmail.com>
2024-06-18 16:57:28 +0530

    added twitter x icon

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,7 +8,10 @@ const Home = () => {
                     <ul className="list-unstyled social-icons mt-4">
                         <li><a href="https://www.linkedin.com/in/gaurav-kumar-3ba537170/" target="_blank" className="fa fa-linkedin"></a></li>
                         <li><a href="https://github.com/gkumar9891" target="_blank" className="fa fa-github"></a></li>
-                        <li><a href="https://x.com/gauravk9891" target="_blank" className="fa fa-twitter"></a></li>
+                        <li><a href="https://x.com/gauravk9891" target="_blank" className="fa fa-x-twitter">
+                            <svg width="15px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
+                            </svg></a></li>
                         <li><a href="Gaurav_Resume.pdf" target="_blank" className="btn btn-secondary rounded-pill">Resume</a></li>
                     </ul>
                 </div>

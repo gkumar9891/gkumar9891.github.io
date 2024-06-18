29f07cdea9a0e359f6de5b84fd6d86d7ba831e03
gkumar9891 <gauravkumargk9892@gmail.com>
2024-06-18 16:25:53 +0530

    initial-commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Home from './views/Home'
+
+function App() {
+  return (
+    <>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ src/views/Home.tsx
@@ -0,0 +1,20 @@
+const Home = () => {
+    return (
+        <>
+            <section className="section banner">
+                <div className="content">
+                    <h2>Gaurav Kumar</h2>
+                    <p>👋 Hi there! I'm Gaurav, a dedicated frontend developer with a love for crafting engaging and user-centric web experiences. With expertise in React.js, Vue.js, JavaScript, HTML, and CSS, I bring over 4 years of hands-on experience in building responsive and dynamic web applications. </p>
+                    <ul className="list-unstyled social-icons mt-4">
+                        <li><a href="https://www.linkedin.com/in/gaurav-kumar-3ba537170/" target="_blank" className="fa fa-linkedin"></a></li>
+                        <li><a href="https://github.com/gkumar9891" target="_blank" className="fa fa-github"></a></li>
+                        <li><a href="https://x.com/gauravk9891" target="_blank" className="fa fa-twitter"></a></li>
+                        <li><a href="Gaurav_Resume.pdf" target="_blank" className="btn btn-secondary rounded-pill">Resume</a></li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Home;

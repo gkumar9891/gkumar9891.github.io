7a652f57fb8ae4e1bd3a3109186f692be6b22657
gkumar9891 <gauravkumargk9892@gmail.com>
2025-06-02 22:53:05 +0530

    changed experience and skill

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -97,7 +97,7 @@ const Home = () => {
                 <ParticleWrapper />
                 <div className="content">
                     <h2>Gaurav Kumar</h2>
-                    <p>👋 Hi there! I'm Gaurav, a dedicated frontend developer with a love for crafting engaging and user-centric web experiences. With expertise in React.js, Vue.js, JavaScript, HTML, and CSS, I bring over 4 years of hands-on experience in building responsive and dynamic web applications. </p>
+                    <p>👋 Hi there! I'm Gaurav, a dedicated full stack developer with a love for crafting engaging and user-centric web experiences. With expertise in React.js, Vue.js, JavaScript, Java, Spring Boot, HTML, and CSS, I bring over 5 years of hands-on experience in building responsive and dynamic web applications. </p>
                     <ul className="list-unstyled social-icons mt-4">
                         <li><a href="https://www.linkedin.com/in/gaurav-kumar-3ba537170/" target="_blank" className="fa fa-linkedin"></a></li>
                         <li><a href="https://github.com/gkumar9891" target="_blank" className="fa fa-github"></a></li>
